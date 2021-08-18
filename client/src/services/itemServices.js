7234c84aca96cf29c919c25c0d1ba60b1a032993
Mete Guneysel <meteg66@gmail.com>
2021-08-18 10:21:32 +0300

    Handle server calls for items

--- client/src/services/itemServices.js
+++ client/src/services/itemServices.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseUrl = "/api/items";
+const url = "/api/items";
 
-export const createItem = (payload) => axios.post(baseUrl, payload);
-export const readFreeItems = () => axios.get(`${baseUrl}/free`);
-export const readTakenItems = () => axios.get(`${baseUrl}/taken`);
-export const releaseItem = (id) => axios.get(`${baseUrl}/release/${id}`);
-export const takeItem = (id) => axios.get(`${baseUrl}/take/${id}`);
+export const createItem = (payload) => axios.post(url, payload);
+export const readFreeItems = () => axios.get(`${url}/free`);
+export const readTakenItems = () => axios.get(`${url}/taken`);
+export const releaseItem = (id) => axios.get(`${url}/release/${id}`);
+export const takeItem = (id) => axios.get(`${url}/take/${id}`);

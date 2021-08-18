119c684fce6830dd72f781e84fbfc7e2cf1da856
Mete Guneysel <meteg66@gmail.com>
2021-08-18 10:26:02 +0300

    Refactored to use useDispatch hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,43 @@
 import jwt_decode from "jwt-decode";
 import React from "react";
-import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import setAuthToken from "./utils/setAuthToken";
-import store from "./store";
 
 import AppBar from "./components/AppBar";
 import Listing from "./components/Listing"; // Items listing
-import LogIn from "./components/LogIn";
+import LogIn from "./components/Forms/LogIn";
 import PrivateRoute from "./components/PrivateRoute";
-import Register from "./components/Register";
+import Register from "./components/Forms/Register";
 import Stash from "./components/Stash"; // TODO: Staging area
-
-// Check for token fermentation to keep user logged in
-if (localStorage.jwtToken) {
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  const decoded = jwt_decode(token);
-  store.dispatch(setCurrentUser(decoded));
-  const currentTime = Date.now() / 1000;
-  if (decoded.exp < currentTime) {
-    store.dispatch(logoutUser());
-    window.location.href = "./login";
-  }
-}
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  // Check for token fermentation to keep user logged in
+  if (localStorage.jwtToken) {
+    const token = localStorage.jwtToken;
+    setAuthToken(token);
+    const decoded = jwt_decode(token);
+    dispatch(setCurrentUser(decoded));
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+      dispatch(logoutUser());
+      window.location.href = "./login";
+    }
+  }
   return (
-    <Provider store={store}>
-      <Router>
-        <AppBar />
-        <Switch>
-          <Route exact path={["/", "/login"]} component={LogIn} />
-          <Route exact path="/register" component={Register} />
-          <PrivateRoute exact path="/listing" component={Listing} />
-          <PrivateRoute exact path="/stash" component={Stash} />
-        </Switch>
-      </Router>
-    </Provider>
+    <Router>
+      <AppBar />
+      <Switch>
+        <Route exact path={["/", "/login"]} component={LogIn} />
+        <Route exact path="/register" component={Register} />
+        <PrivateRoute exact path="/listing" component={Listing} />
+        <PrivateRoute exact path="/stash" component={Stash} />
+      </Switch>
+    </Router>
   );
 };
 

50d685a3003916722a561273575aedc01350bada
Mete Guneysel <meteg66@gmail.com>
2021-08-18 10:14:05 +0300

    Wrapped 'App' with redux store provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import store from "./store";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

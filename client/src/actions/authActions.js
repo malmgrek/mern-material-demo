e7a15e16b6c8b1cc31b246bd3948feabe73b2731
Mete Guneysel <meteg66@gmail.com>
2021-08-18 10:23:30 +0300

    Changed actions to async/await

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,49 +1,49 @@
-import axios from "axios";
 import jwt_decode from "jwt-decode";
+import { login, register } from "../services/userServices";
 
 import setAuthToken from "../utils/setAuthToken";
-import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
-
-export const registerUser = (userData, history) => dispatch => {
-  axios
-    .post("/api/users/register", userData)
-    .then(res => history.push("/login"))
-    .catch(err =>
+export const registerUser = (userData, history) => {
+  return async (dispatch) => {
+    try {
+      await register(userData);
+      history.push("/login");
+    } catch (err) {
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+        payload: err.response.data,
+      });
+    }
+  };
 };
 
-
-export const loginUser = userData => dispatch => {
-  axios
-    .post("/api/users/login", userData)
-    .then(res => {
+export const loginUser = (userData) => {
+  return async (dispatch) => {
+    try {
+      const res = await login(userData);
       const { token } = res.data;
       localStorage.setItem("jwtToken", token);
       setAuthToken(token);
       const decoded = jwt_decode(token);
       dispatch(setCurrentUser(decoded));
-    })
-    .catch(err => dispatch({
-      type: GET_ERRORS,
-      payload: err.response.data
-    }));
+    } catch (err) {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      });
+    }
+  };
 };
 
-
-export const setCurrentUser = decoded => {
+export const setCurrentUser = (decoded) => {
   return {
     type: SET_CURRENT_USER,
-    payload: decoded
-  }
+    payload: decoded,
+  };
 };
 
-
-export const logoutUser = () => dispatch => {
+export const logoutUser = () => (dispatch) => {
   localStorage.removeItem("jwtToken");
   setAuthToken(false);
   dispatch(setCurrentUser({}));

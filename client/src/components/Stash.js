965da83bd322ae429aeab729dc94d35e822904c2
Mete Guneysel <meteg66@gmail.com>
2021-08-18 10:31:34 +0300

    Basket content is now stored to localStorage

--- client/src/components/Stash.js
+++ client/src/components/Stash.js
@@ -1,40 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import { DataGrid } from "@material-ui/data-grid";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import DeleteIcon from "@material-ui/icons/Delete";
 import IconButton from "@material-ui/core/IconButton";
 
-import api from "../api";
+import { useDispatch, useSelector } from "react-redux";
+import { getReservations, removeItem } from "../actions/itemActions";
 
-// TODO: Change to functional form
-// TODO: Change basket contents to localStorage
 // NOTE: Interesting reading
 //       - State or props? https://reactjs.org/docs/faq-state.html#what-is-the-difference-between-state-and-props
 //       - Especially https://github.com/uberVU/react-guide/blob/master/props-vs-state.md
 
 const Stash = () => {
-  const [rows, setRows] = useState(null);
+  const dispatch = useDispatch();
+  const { reservations } = useSelector((state) => state.items);
 
   useEffect(() => {
-    const checkTakenItems = async () => {
-      const {
-        data: { data },
-      } = await api.readTakenItems();
-      setRows(
-        data.map((item) => ({
-          id: item._id,
-          name: item.name,
-          rating: item.rating,
-        }))
-      );
-    };
-    checkTakenItems();
-  }, []);
+    dispatch(getReservations());
+  }, [dispatch]);
 
-  async function handleDeleteRow(id) {
-    const { data } = await api.releaseItem(id);
-    setRows(rows.filter((item) => item.id !== data.id));
+  function handleDeleteRow(id) {
+    dispatch(removeItem(id));
   }
 
   return (
@@ -42,46 +29,44 @@ const Stash = () => {
       <CssBaseline />
       <main>
         <div style={{ height: 400, width: "90%", margin: "auto" }}>
-          {!rows ? null : (
-            <DataGrid
-              rows={rows}
-              columns={[
-                { field: "name", headerName: "Name", width: 300 },
-                {
-                  field: "rating",
-                  headerName: "Rating",
-                  width: 130,
-                  type: "number",
-                },
-                {
-                  field: "actions",
-                  headerName: "Actions",
-                  sortable: false,
-                  width: 150,
-                  renderCell: (params) => (
-                    <IconButton
-                      aria-label="delete"
-                      onClick={() => handleDeleteRow(params.row.id)}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  ),
-                },
-                // NOTE: a value getter like below is supported
-                // {
-                //   field: "fullName",
-                //   headerName: "Full name",
-                //   description: "This column has a value getter and is not sortable.",
-                //   sortable: false,
-                //   width: 160,
-                //   valueGetter: (params) =>
-                //     `${params.getValue("firstName") || ""} ${params.getValue("lastName") || ""}`,
-                // },
-              ]}
-              pageSize={10}
-              /* checkboxSelection */
-            />
-          )}
+          <DataGrid
+            rows={reservations ? reservations : []}
+            columns={[
+              { field: "name", headerName: "Name", width: 300 },
+              {
+                field: "rating",
+                headerName: "Rating",
+                width: 130,
+                type: "number",
+              },
+              {
+                field: "actions",
+                headerName: "Actions",
+                sortable: false,
+                width: 150,
+                renderCell: (params) => (
+                  <IconButton
+                    aria-label="delete"
+                    onClick={() => handleDeleteRow(params.row.id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                ),
+              },
+              // NOTE: a value getter like below is supported
+              // {
+              //   field: "fullName",
+              //   headerName: "Full name",
+              //   description: "This column has a value getter and is not sortable.",
+              //   sortable: false,
+              //   width: 160,
+              //   valueGetter: (params) =>
+              //     `${params.getValue("firstName") || ""} ${params.getValue("lastName") || ""}`,
+              // },
+            ]}
+            pageSize={10}
+            /* checkboxSelection */
+          />
         </div>
       </main>
     </>

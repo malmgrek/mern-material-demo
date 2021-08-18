36cd01a39389d9274cfe89a632711a3a2ed1101b
Mete Guneysel <meteg66@gmail.com>
2021-08-18 09:55:42 +0300

    Added 2 more items with valid  url for random images

--- server/scripts/populate-db.js
+++ server/scripts/populate-db.js
@@ -3,50 +3,63 @@ const http = require("http");
 const PORT = 3000;
 const HOST = "localhost";
 
-
 /*
  * Create some service items to DB
  */
 const populate = () => {
-
   const items = [
     {
-        name: "foobar",
-        description: "Test 1",
-        imgUrl: "https://commons.wikimedia.org/wiki/Special:Random/Image",
-        rating: 4
+      name: "foobar",
+      description: "Test 1",
+      imgUrl: "https://picsum.photos/200",
+      rating: 4,
     },
     {
-        name: "barfoo",
-        description: "Test 2",
-        imgUrl: "https://commons.wikimedia.org/wiki/Special:Random/Image",
-        rating: 3
+      name: "barfoo",
+      description: "Test 2",
+      imgUrl: "https://picsum.photos/200",
+      rating: 3,
+    },
+    {
+      name: "barfoo21",
+      description: "Test 3",
+      imgUrl: "https://picsum.photos/200",
+      rating: 5,
+    },
+    {
+      name: "barfoo22",
+      description: "Test 4",
+      imgUrl: "https://picsum.photos/200",
+      rating: 1,
     },
   ];
 
   // NOTE: If you want (at some point) to process the responses further, do it functionally
   // in the pipeline with suitable callbacks
-  items.map(item => {
+  items.map((item) => {
     const payload = JSON.stringify(item);
-    http.request({
-      hostname: HOST,
-      path: "/api/items",
-      port: PORT,
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Content-Length": payload.length
-      }
-    }, res => {
-      console.log(`statusCode: ${res.statusCode}`);
-      res.on("data", d => {
-        process.stdout.write(d)
-      });
-    }).on(
-      "error", err => console.log(err)
-    ).write(payload)
-  })
-
-}
+    http
+      .request(
+        {
+          hostname: HOST,
+          path: "/api/items",
+          port: PORT,
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Content-Length": payload.length,
+          },
+        },
+        (res) => {
+          console.log(`statusCode: ${res.statusCode}`);
+          res.on("data", (d) => {
+            process.stdout.write(d);
+          });
+        }
+      )
+      .on("error", (err) => console.log(err))
+      .write(payload);
+  });
+};
 
 populate();

99675bcd3c9b5ef2f2df1e37e2164c22a1126bd5
Mete Guneysel <meteg66@gmail.com>
2021-08-18 10:12:37 +0300

    Added types for itemReducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,8 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+
+export const GET_ITEMS = "GET_ITEMS";
+export const ADD_ITEM = "ADD_ITEM";
+export const REMOVE_ITEM = "REMOVE_ITEM";
+export const GET_ADDED_ITEMS = "GET_ADDED_ITEMS";
